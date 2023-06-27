16e2daa188d499d8b2b062663e67fd6e95e59ae0
Daniel Soifer <daniel.soifer@codefresh.io>
2023-06-27 13:00:59 +0300

    .

--- main.py
+++ main.py
@@ -6,33 +6,39 @@ from agents import linkedin_lookup_agent
 from third_parties import scrape_linkedin_profile
 
 
-prompt = PromptTemplate(
-    input_variables=["information"],
-    template="""
-        Given the following information: {information}
-        
-        Your goal is to write:
-         1. a summary of the information.
-         2. two key takeaways from the information.
-         
-         No need to describe the information in detail, just summarize it and write two key takeaways.
-    """,
-)
+def who_is(name: str):
+    prompt = PromptTemplate(
+        input_variables=["information"],
+        template="""
+            Given the following information: {information}
+            
+            Your goal is to write:
+             1. a summary of the information.
+             2. two key takeaways from the information.
+             
+             No need to describe the information in detail, just summarize it and write two key takeaways.
+        """,
+    )
 
-llm = ChatOpenAI(
-    temperature=0.0,
-    # max_tokens=150,
-    model="gpt-3.5-turbo",
-)
-chain = LLMChain(llm=llm, prompt=prompt)
+    llm = ChatOpenAI(
+        temperature=0.0,
+        # max_tokens=150,
+        model="gpt-3.5-turbo",
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
 
-linkedin_profile_url = linkedin_lookup_agent(
-    person_name="Daniel Soifer Israel Team Lead"
-)
-linkedin_profile = scrape_linkedin_profile(linkedin_profile_url)
+    linkedin_profile_url = linkedin_lookup_agent(
+        person_name=name,
+    )
+    linkedin_profile = scrape_linkedin_profile(linkedin_profile_url)
 
-result = chain.run(information=linkedin_profile)
+    result = chain.run(information=linkedin_profile)
 
-print(result)
-with open("output.txt", "w") as file:
-    file.write(result)
+    print(result)
+    with open("output.txt", "w") as file:
+        file.write(result)
+
+
+if __name__ == "__main__":
+    user_input = input("Enter the name of the person with additional context:")
+    who_is(user_input)
